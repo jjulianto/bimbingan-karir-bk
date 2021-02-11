30bfd38abf536a484c9054db16ecc2091a04943e
Julianto08 <jjulianto08@gmail.com>
2021-02-11 18:30:29 +0700

    Add chatting modal

--- app/Views/dashboard/konsultasi.php
+++ app/Views/dashboard/konsultasi.php
@@ -19,6 +19,22 @@
         </div>
     </div>
     <div class="row mb-4">
+        <a href="#" data-toggle="modal" data-target="#chattingModal" class="custom-card">
+            <div class="col-md-12 card-konten">
+                <div class="item">
+                    <div class="featured-image mb-3">
+                        <img src="<?= base_url(); ?>/assets-dashboard/img/undraw_profile_3.svg" alt="This is a cool picture" />
+                    </div>
+                    <div class="description">
+                        <h5 class="font-weight-bold">Senku Ishigami</h5>
+                        <p class="text-muted">Tanggal : 08 July 2020</p>
+                        <span class="badge status badge-danger"> Belum Selesai</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="row mb-4">
         <div class="col-md-12 card-konten">
             <div class="item">
                 <div class="featured-image mb-3">
@@ -32,16 +48,93 @@
             </div>
         </div>
     </div>
-    <div class="row mb-4">
-        <div class="col-md-12 card-konten">
-            <div class="item">
-                <div class="featured-image mb-3">
-                    <img src="<?= base_url(); ?>/assets-dashboard/img/undraw_profile_3.svg" alt="This is a cool picture" />
+</div>
+
+<div class="modal modal-konsultasi fade" id="chattingModal" tabindex="-1" aria-labelledby="chattingModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content" id="konsultasiContent">
+            <div class="modal-header" id="konsultasiHeader">
+                <div class="msg-header-img">
+                    <img src="<?= base_url(); ?>/assets-dashboard/img/undraw_profile_3.svg" class="profile" alt="Cool">
                 </div>
-                <div class="description">
-                    <h5 class="font-weight-bold">Senku Ishigami</h5>
-                    <p class="text-muted">Tanggal : 08 July 2020</p>
-                    <span class="badge status badge-danger"> Belum Selesai</span>
+                <div class="active">
+                    <p>Senku Ishigami</p>
+                </div>
+                <div class="header-icons">
+                    <i class="fas fa-ellipsis-v"></i>
+                    <a href="#" data-toggle="modal" data-dismiss="modal" aria-label="Close">
+                        <i class="fas fa-times"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="modal-body" id="konsultasiBody">
+                <div class="msg-inbox">
+                    <div class="chats">
+                        <div class="msg-page">
+                            <div class="received-chats">
+                                <div class="received-chats-img">
+                                    <img src="<?= base_url(); ?>/assets-dashboard/img/undraw_profile_3.svg" alt="Guru">
+                                </div>
+                                <div class="received-msg">
+                                    <div class="received-msg-inbox">
+                                        <p>Selamat siang, pak</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="outgoing-chats">
+                                <div class="outgoing-msg-inbox">
+                                    <p>Selamat siang</p>
+                                </div>
+                                <div class="outgoing-chats-img">
+                                    <img src="<?= base_url(); ?>/assets-dashboard/img/undraw_profile_2.svg" alt="Guru">
+                                </div>
+                            </div>
+                            <div class="received-chats">
+                                <div class="received-chats-img">
+                                    <img src="<?= base_url(); ?>/assets-dashboard/img/undraw_profile_3.svg" alt="Guru">
+                                </div>
+                                <div class="received-msg">
+                                    <div class="received-msg-inbox">
+                                         <p>Mohon maaf mengganggu waktunya sebentar, pak</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="outgoing-chats">
+                                    <div class="outgoing-msg-inbox">
+                                        <p>Oh iya, tidak apa-apa</p>
+                                    </div>
+                                <div class="outgoing-chats-img">
+                                    <img src="<?= base_url(); ?>/assets-dashboard/img/undraw_profile_2.svg" alt="Guru">
+                                </div>
+                            </div>
+                            <div class="outgoing-chats">
+                                    <div class="outgoing-msg-inbox">
+                                        <p>Ada apa di waktu hari ini?</p>
+                                    </div>
+                                <div class="outgoing-chats-img">
+                                    <img src="<?= base_url(); ?>/assets-dashboard/img/undraw_profile_2.svg" alt="Guru">
+                                </div>
+                            </div>
+                            <div class="received-chats">
+                                <div class="received-chats-img">
+                                    <img src="<?= base_url(); ?>/assets-dashboard/img/undraw_profile_3.svg" alt="Guru">
+                                </div>
+                                <div class="received-msg">
+                                    <div class="received-msg-inbox">
+                                        <p>Maaf pak, saya sedikit bermasalah dengan kehidupan saya</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer" id="konsultasiFooter">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Tulis pesan disini...">
+                    <div class="input-group-append">
+                        <span class="input-group-text"><i class="fas fa-paper-plane"></i></span>
+                    </div>
                 </div>
             </div>
         </div>
--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -54,17 +54,17 @@
     </li>
 </ul>
 <!-- End of Sidebar -->
-<!-- Register Modal -->
-<div class="modal modal-login fade" id="logoutModal" tabindex="-1" aria-labelledby="registerModal" aria-hidden="true">
+<!-- Logout Modal -->
+<div class="modal modal-login fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModal" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header text-center">
+        <div class="modal-content" id="loginContent">
+            <div class="modal-header" id="loginHeader">
                 <h5 class="modal-title font-weight-bold" id="exampleModalLabel">BK SMKN 1 CIMAHI</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body text-center py-3">
+            <div class="modal-body py-3" id="loginBody">
                 <p>Apakah anda yakin untuk keluar <br /> dari website ini?</p>
                 <a href="#"><button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-times"></i> Cancel</button></a>
                 <a href="/"><button type="button" class="btn btn-primary"><i class="fas fa-sign-out-alt"></i> Logout</button></a>
